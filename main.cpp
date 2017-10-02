94badddc7a8570c6b5d18042852999cb2869a259
jblackbelt <31226129+jblackbelt@users.noreply.github.com>
2017-10-02 16:29:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
   // use imput to set up a bitmap
   // check and varify that bitmap is setup correctly
   // repeat (check to see which value (R,G,B) is the highest & set all vars to the highest var) while you go through the bitmap
+  // call fuction to change to an .bmp
   // output bitmap to OldTimey.bmp
   return 0;
 }
