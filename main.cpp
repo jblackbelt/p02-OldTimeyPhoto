fa1dc567b2b1fbb8ea3205182a7b048f8f8f750d
jblackbelt <31226129+jblackbelt@users.noreply.github.com>
2017-10-02 16:23:42 -0700

    planning project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <vectors>
+#include <strings>
+#include "bitmap.h"
+
+int main()
+{
+  // setup vector and imput var
+  // ask user for a file to imput 
+  // use imput to set up a bitmap
+  // check and varify that bitmap is setup correctly
+  // repeat (check to see which value (R,G,B) is the highest & set all vars to the highest var) while you go through the bitmap
+  // output bitmap to OldTimey.bmp
+  return 0;
+}
