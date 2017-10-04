cbf1cde4549252579701b1f3405ae286906a2b41
jblackbelt <jwest27.mail.csuchico.edu>
2017-10-04 16:30:47 -0700

    did funtion open

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
-#include <vectors>
-#include <strings>
+#include <vector>
+#include <string>
 #include "bitmap.h"
+#include <iostream>
+
+using namespace std;
+
 
 int main()
 {
-  // setup vector and imput var
-  // ask user for a file to imput 
-  // use imput to set up a bitmap
-  // check and varify that bitmap is setup correctly
-  // repeat (check to see which value (R,G,B) is the highest & set all vars to the highest var) while you go through the bitmap
-  // call fuction to change to an .bmp
-  // output bitmap to OldTimey.bmp
-  return 0;
+    string pic = "machupicchu.bmp";
+    Bitmap image;
+
+    vector < vector < Pixel > > bmp;
+    image.open(pic);
+    bmp = image.toPixelMatrix();
+
+    // setup vector and imput var
+    // ask user for a file to imput 
+    // use imput to set up a bitmap
+    // check and varify that bitmap is setup correctly
+    // repeat (check to see which value (R,G,B) is the highest & set all vars to the highest var) while you go through the bitmap
+    // call fuction to change to an .bmp
+    // output bitmap to OldTimey.bmp
+  
+    return 0;
 }
