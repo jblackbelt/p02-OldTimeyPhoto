be29eba88ff89263220ae0ee3715a2e243b847fa
jblackbelt <jwest27.mail.csuchico.edu>
2017-10-10 13:06:09 -0700

    maybe did the right thing

--- main.cpp
+++ main.cpp
@@ -13,34 +13,23 @@ int main()
     vector < vector < Pixel > > bmp;
     image.open(pic);
     bmp = image.toPixelMatrix();
-    cout << "row# " << bmp.size() << "  col# " << bmp[0].size() << endl; 
     for ( int i = 0; i < bmp.size(); i++)
     {
         for ( int x = 0; x < bmp[0].size(); x++)
         {
             Pixel rgb = bmp[i][x];
-            if( rgb.red> rgb.green && rgb.red > rgb.blue)
-            {
-                rgb.green = rgb.red;
-                rgb.blue = rgb.red;
-            }
-            else if (rgb.green > rgb.red && rgb.green > rgb.blue)
-            {
-                rgb.red = rgb.green;
-                rgb.blue = rgb.green;
-            }
-            else
-            {
-                rgb.green = rgb.blue;
-                rgb.red = rgb.blue;
-            }
+            rgb.red = (bmp[i][x].red + bmp[i][x].green + bmp[i][x].blue)/2;
+            rgb.green = (bmp[i][x].red + bmp[i][x].green + bmp[i][x].blue)/2;
+            rgb.blue = (bmp[i][x].red + bmp[i][x].green + bmp[i][x].blue)/2;
+            bmp[i][x] = rgb;
         }
     }
+    image.save("oldtimey.bmp");
     // setup vector and imput var
     // ask user for a file to imput 
     // use imput to set up a bitmap
     // check and varify that bitmap is setup correctly
-    // repeat (check to see which value (R,G,B) is the highest & set all vars to the highest var) while you go through the bitmap
+    // repeat (set all vars to the average of the vars ) while you go through the bitmap
     // call fuction to change to an .bmp
     // output bitmap to OldTimey.bmp
   
